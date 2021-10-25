52b7ab379f270444ecec36b7af9fec1e49bb73e2
Sven Lindstroem <svenlindstroem@gmail.com>
2021-10-25 14:10:55 +0200

    init serviceworker

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import App from "./App.svelte";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    // register service worker
+    navigator.serviceWorker.register("worker.js").then(
+      function (registration) {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
+
 const app = new App({
   target: document.body,
 });
