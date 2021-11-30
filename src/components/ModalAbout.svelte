095354b2a6f6aecc5c070d82dd56d29ea4508153
Sven Lindstroem <svenlindstroem@gmail.com>
2021-11-30 18:11:00 +0100

    adding modal about

---
+++ src/components/ModalAbout.svelte
@@ -0,0 +1,35 @@
+<div
+  id="modal-about"
+  class="modal top-sheet about-top-sheet"
+  role="dialog"
+  aria-labelledby="shopping-list-about"
+>
+  <div class="col s12 white">
+    <div class="modal-content">
+      <h5 id="shopping-list-about">About</h5>
+      <div class="row">
+        <div class="col s12">
+          <p class="primary-text darker">
+            <a
+              href="https://github.com/ibm-watson-data-lab/shopping-list"
+              target="_blank"
+              >Shopping List is a series of Offline First demo apps, each built
+              using a different stack.</a
+            >
+            These demo apps cover Progressive Web Apps, hybrid mobile apps, native
+            mobile apps, and desktop apps. This particular demo app is a
+            <strong>Progressive Web App</strong>
+            built using <strong>Vanilla JS and PouchDB</strong>.
+            <a
+              href="https://github.com/ibm-watson-data-lab/shopping-list-vanillajs-pouchdb"
+              target="_blank">Get the source code</a
+            >.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="modal-footer primary-color">
+      <button class="btn-flat modal-close" type="button">Close</button>
+    </div>
+  </div>
+</div>
