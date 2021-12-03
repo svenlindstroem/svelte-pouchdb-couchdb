9220ffd1a89cd4ad005c2bda08d6f0fd38c3336d
Sven Lindstroem <svenlindstroem@gmail.com>
2021-12-03 13:05:09 +0100

    adding helper file

---
+++ src/helper.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * @param {*} element
+ * set focus on input element and
+ * move carret on input dom element to
+ * end of input text (if any)
+ */
+function focusHelper(element) {
+  console.log(Object.prototype.toString.call(element));
+  element.focus();
+  if (typeof element.selectionStart == "number") {
+    element.selectionStart = element.selectionEnd = element.value.length;
+  } else if (typeof element.createTextRange != "undefined") {
+    var range = element.createTextRange();
+    range.collapse(false);
+    range.select();
+  }
+}
+
+export default focusHelper;
