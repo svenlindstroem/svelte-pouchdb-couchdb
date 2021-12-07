c5afe9400756cfd48e9ff2c9f6f5836849af5499
Sven Lindstroem <svenlindstroem@gmail.com>
2021-12-07 18:11:05 +0100

    online offline css implemented

--- src/App.svelte
+++ src/App.svelte
@@ -15,6 +15,7 @@
   // PouchDB.debug.disable();
   // localDb.on("error", function (err) {debugger;});
 
+  let online; // listen to online / offline event on window
   let lists = [];
   let items = [];
   let currentView = "master-view";
@@ -60,75 +61,87 @@
   getLists();
 </script>
 
-<!-- banner -->
-<header class="navbar-fixed">
-  <nav id="nav" class="primary-color">
-    <div class="nav-wrapper">
-      <span class="brand-logo left">
-        {#if currentView === "detail-view"}
-          <a href="#!" on:click={() => (currentView = "master-view")}
-            ><i class="material-icons">arrow_back</i></a
-          >
-        {/if}
-        <span id="header-title">
+<svelte:window bind:online />
+<!-- 
+  /**
+  * There is no elegent way to target body class online / offline. 
+  * But we can use the section tag instead of the body tag to wrap the app
+  */
+-->
+<section class={online ? "" : "offline"}>
+  <!-- banner -->
+  <header class="navbar-fixed">
+    <nav id="nav" class="primary-color">
+      <div class="nav-wrapper">
+        <span class="brand-logo left {currentView}">
           {#if currentView === "detail-view"}
-            {$currentList.title}
-          {:else}
-            Shopping Lists
+            <a
+              class="test"
+              href="#!"
+              on:click={() => (currentView = "master-view")}
+              ><i class="material-icons">arrow_back</i></a
+            >
           {/if}
+
+          <span id="header-title">
+            {#if currentView === "detail-view"}
+              {$currentList.title}
+            {:else}
+              Shopping Lists
+            {/if}
+          </span>
         </span>
-      </span>
-      <!-- settings button -->
-      <a
-        href="#modal-about"
-        class="waves-effect waves-light modal-trigger right settings"
-        ><i class="material-icons">info_outline</i></a
-      >
-      <a
-        href="#modal-settings"
-        class="waves-effect waves-light modal-trigger right settings"
-        ><i class="material-icons">settings</i></a
-      >
+        <!-- settings button -->
+        <a
+          href="#modal-about"
+          class="waves-effect waves-light modal-trigger right settings"
+          ><i class="material-icons">info_outline</i></a
+        >
+        <a
+          href="#modal-settings"
+          class="waves-effect waves-light modal-trigger right settings"
+          ><i class="material-icons">settings</i></a
+        >
+      </div>
+    </nav>
+  </header>
+  <!-- content area -->
+  <main class={currentView}>
+    <!-- shopping lists get inserted here -->
+    <div id="shopping-lists">
+      {#await lists}
+        ... loading
+      {:then lists}
+        {#each lists as list}
+          <List {list} {localDb} />
+        {/each}
+      {/await}
     </div>
-  </nav>
-</header>
-<!-- content area -->
-<main class={currentView}>
-  <!-- shopping lists get inserted here -->
-  <div id="shopping-lists">
-    {#await lists}
-      ... loading
-    {:then lists}
-      {#each lists as list}
-        <List {list} {localDb} />
+
+    <ul id="shopping-list-items">
+      <!-- shopping list items get inserted here -->
+      {#each items as item}
+        <Item {localDb} {item} />
       {/each}
-    {/await}
-  </div>
+    </ul>
+  </main>
+  <!-- add more stuff button -->
+  <button
+    class="fixed-action-btn btn-floating btn-large secondary-color modal-trigger"
+    data-target="modal-add"
+  >
+    <i class="large material-icons">add</i>
+  </button>
 
-  <ul id="shopping-list-items">
-    <!-- shopping list items get inserted here -->
-    {#each items as item}
-      <Item {localDb} {item} />
-    {/each}
-  </ul>
-</main>
+  <!-- modal: add a shopping list settings form -->
+  <ModalSettings {localDb} />
 
-<!-- add more stuff button -->
-<button
-  class="fixed-action-btn btn-floating btn-large secondary-color modal-trigger"
-  data-target="modal-add"
->
-  <i class="large material-icons">add</i>
-</button>
+  <!-- modal: open shopping list about -->
+  <ModalAbout />
 
-<!-- modal: add a shopping list settings form -->
-<ModalSettings {localDb} />
-
-<!-- modal: open shopping list about -->
-<ModalAbout />
-
-<!-- modal: add a shopping list or an item form -->
-<ModalAdd {localDb} />
+  <!-- modal: add a shopping list or an item form -->
+  <ModalAdd {localDb} />
+</section>
 
 <style>
 </style>
