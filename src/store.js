3ae2e2d128226a1c911821ad2238b1c4a83d4b59
Sven Lindstroem <svenlindstroem@gmail.com>
2022-06-10 13:55:08 +0200

    need syncError after all

--- src/store.js
+++ src/store.js
@@ -4,5 +4,5 @@ export const lastLocalModification = writable(0);
 //export const settings = writable({});
 
 // syncError is when the remoteDB is not reachable
-// export const syncError = writable(0);
+export const syncError = writable(0);
 // export const settings = writable(0);
