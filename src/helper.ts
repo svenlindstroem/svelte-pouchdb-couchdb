da5ad2e235fda650ec88b129624179ee1044de26
Sven Lindstroem <svenlindstroem@gmail.com>
2022-06-15 19:12:49 +0200

    typescripting helper js

--- src/helper.ts
+++ src/helper.ts
@@ -1,16 +1,16 @@
 /**
- *
- * @param {*} element
  * set focus on input element and
  * move carret on input dom element to
  * end of input text (if any)
+ *  @param {*} element
  */
-function focusHelper(element) {
+function focusHelper(element: HTMLInputElement | HTMLTextAreaElement): void {
   // console.log(Object.prototype.toString.call(element));
+  if(!element) return;
   element.focus();
-  if (typeof element.selectionStart == "number") {
+  if (element.selectionStart && typeof element.selectionStart == "number") {
     element.selectionStart = element.selectionEnd = element.value.length;
-  } else if (typeof element.createTextRange != "undefined") {
+  } else if (element.createTextRange && typeof element.createTextRange != "undefined") {
     var range = element.createTextRange();
     range.collapse(false);
     range.select();
