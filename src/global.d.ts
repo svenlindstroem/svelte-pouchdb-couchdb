56daeeaeb97d7cfd0cb9db8bece80594aeeff8bd
Sven Lindstroem <svenlindstroem@gmail.com>
2022-06-15 19:15:05 +0200

    adding a global

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="svelte" />
+
+export {};
+declare global {
+  /**
+   * HTMLTextAreaElement.createTextRange function is microsoft specific 
+   */
+  interface Element {
+    createTextRange: any;
+  }
+}
